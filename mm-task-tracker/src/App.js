95a96758ba3bc26686ca34638ca9c22b0b13c940
alex <alexandersegerev@gmail.com>
2024-08-22 15:40:07 -0400

    Added the Rundown

--- mm-task-tracker/src/App.js
+++ mm-task-tracker/src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+import {useState} from 'react'
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      text: "Talk about Google's 20th IPO Anniversary",
+      isCompleted: false,
+    },
+
+    {
+      text: 'Talk To PaloAlto CEO Nikesh Arora',
+      date: '06:16:38 PM',
+      isCompleted: false,
+    },
+    {
+      text: 'Reveal the Name Of the 700% Returner',
+      date: '06:27:38 PM',
+      isCompleted: false,
+    },
+    {
+      text: 'Talk To Cardinal Health CEO Jason Hollar',
+      date: '06:37:38 PM',
+      isCompleted: false,
+    },
+
+    {
+      text: 'Lightning Round',
+      date: '06:45:38 PM',
+      isCompleted: false,
+    },
+
+    {
+        text: 'Talk About What Stocks To Watch',
+        date: '06:53:38 PM',
+        isCompleted: false,
+      },
+
+  ])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> MAD Rundown </h1>
+        {todos.map((todo, i) => <div key = {i}> {todo.text} </div>)}
     </div>
   );
 }
