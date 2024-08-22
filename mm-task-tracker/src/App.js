c6a6312a492aa90a4382c90f96e61917e2c4a4f5
alex <alexandersegerev@gmail.com>
2024-08-22 15:43:48 -0400

    Added the preliminary styling

--- mm-task-tracker/src/App.js
+++ mm-task-tracker/src/App.js
@@ -40,7 +40,7 @@ function App() {
   return (
     <div className="App">
       <h1> MAD Rundown </h1>
-        {todos.map((todo, i) => <div key = {i}> {todo.text} </div>)}
+        {todos.map((todo, i) => <div className="todo" key = {i}> {todo.text} </div>)}
     </div>
   );
 }
